9f9538aded7a9c5b94460fed7cfe85aa84fbfbdf
Tommy Lohil <tommylohil23052000@gmail.com>
2019-05-23 06:28:12 +0700

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -23,12 +23,9 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
     response.json().then((data) =>{
         if (data.error) {
-            // console.log(data.error)
             messageOne.textContent = data.error
         }
         else {
-            // console.log(data.location)
-            // console.log(data.forecast)
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
         }
